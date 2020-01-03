0271feb4b2b9a0bf6bcb87d0f1896d29b30aac62
Milad Abbasi <miladabbasi1997@gmail.com>
2020-01-03 18:51:19 +0330

    Remove tv series links

--- imdbCrawler.js
+++ imdbCrawler.js
@@ -5,141 +5,139 @@ const axios = require('./axios');
 const esClient = require('./elasticsearch');
 
 class IMDBCrawler {
-  constructor(url) {
-    const seedUrls = [
+	constructor(url) {
+		const seedUrls = [
       'movies-coming-soon',
       'movies-in-theaters',
       'chart/top',
       'chart/boxoffice',
       'chart/moviemeter',
       'chart/top-english-movies',
-      'chart/tvmeter',
-      'chart/toptv',
       'chart/bottom'
     ];
 
-    this.seedUrls = seedUrls.map(seedUrl => `${url}/${seedUrl}`);
-  }
+		this.seedUrls = seedUrls.map(seedUrl => `${url}/${seedUrl}`);
+	}
 
-  async extractMoviesLinks(seedUrl) {
-    const { data } = await axios.get(seedUrl);
-    const $ = cheerio.load(data);
-    const moviesLinks = [];
+	async extractMoviesLinks(seedUrl) {
+		const { data } = await axios.get(seedUrl);
+		const $ = cheerio.load(data);
+		const moviesLinks = [];
 
-    $('#main a[title]').each((i, element) => {
-      const link = url.parse($(element).attr('href'));
-      const title = $(element).attr('title');
-      const linkPaths = link.pathname.split('/').filter(Boolean);
+		$('#main a[title]').each((i, element) => {
+			const link = url.parse($(element).attr('href'));
+			const title = $(element).attr('title');
+			const linkPaths = link.pathname.split('/').filter(Boolean);
 
-      if (linkPaths[0] === 'title' && title !== 'Delete') {
-        moviesLinks.push(linkPaths[1]);
-      }
-    });
+			if (linkPaths[0] === 'title' && title !== 'Delete') {
+				moviesLinks.push(linkPaths[1]);
+			}
+		});
 
-    return moviesLinks;
-  }
+		return moviesLinks;
+	}
 
-  async extractMovieDetails(movieId) {
-    const { data } = await axios.get(`/title/${movieId}`);
-    const $ = cheerio.load(data);
-    const content = $('#content-2-wide');
-    const details = {};
+	async extractMovieDetails(movieId) {
+		const { data } = await axios.get(`/title/${movieId}`);
+		const $ = cheerio.load(data);
+		const content = $('#content-2-wide');
+		const details = {};
 
-    const titleAndYear = content.find('#titleYear').parent();
-    details.year = Number(titleAndYear.find('a').text());
-    titleAndYear.find('#titleYear').remove();
-    details.name = titleAndYear.text().trim();
-    details.rating = Number(content.find('div.ratingValue strong span').text());
-    details.image = content.find('div.poster a img').attr('src');
+		const titleAndYear = content.find('#titleYear').parent();
+		details.year = Number(titleAndYear.find('a').text());
+		titleAndYear.find('#titleYear').remove();
+		details.name = titleAndYear.text().trim();
+		details.rating = Number(content.find('div.ratingValue strong span').text());
+		details.image = content.find('div.poster a img').attr('src');
 
-    const summary = content.find('div.plot_summary');
-    details.summary = summary
-      .find('.summary_text')
-      .text()
-      .trim();
-    details.director = summary
-      .children()
-      .eq(1)
-      .find('a')
-      .first()
-      .text();
-    details.writer = summary
-      .children()
-      .eq(2)
-      .find('a')
-      .first()
-      .text();
+		const summary = content.find('div.plot_summary');
+		details.summary = summary
+			.find('.summary_text')
+			.text()
+			.trim();
+		details.director = summary
+			.children()
+			.eq(1)
+			.find('a')
+			.first()
+			.text();
+		details.writer = summary
+			.children()
+			.eq(2)
+			.find('a')
+			.first()
+			.text();
 
-    details.stars = [];
-    const stars = summary
-      .children()
-      .eq(3)
-      .find('a');
-    stars.each((i, element) => {
-      if (i !== stars.length - 1) details.stars.push($(element).text());
-    });
+		details.stars = [];
+		const stars = summary
+			.children()
+			.eq(3)
+			.find('a');
+		stars.each((i, element) => {
+			if (i !== stars.length - 1) details.stars.push($(element).text());
+		});
 
-    details.metascore = Number(content.find('.metacriticScore span').text());
+		details.metascore = Number(content.find('.metacriticScore span').text());
 
-    details.genres = [];
-    const genres = content
-      .find('#titleStoryLine div:contains("Genres")')
-      .find('a');
-    genres.each((i, element) => {
-      details.genres.push(
-        $(element)
-          .text()
-          .trim()
-      );
-    });
+		details.genres = [];
+		const genres = content
+			.find('#titleStoryLine div:contains("Genres")')
+			.find('a');
+		genres.each((i, element) => {
+			details.genres.push(
+				$(element)
+				.text()
+				.trim()
+			);
+		});
 
-    return details;
-  }
+		return details;
+	}
 
-  async crawlAndIndex() {
-    const movieIDs = [];
-    const movieDetails = [];
+	async crawlAndIndex() {
+		const movieIDs = [];
+		const movieDetails = [];
 
-    let seedUrl;
-    while (this.seedUrls.length) {
-      try {
-        seedUrl = this.seedUrls.shift();
-        console.log(`Extracting links from: ${seedUrl}`);
-        const links = await this.extractMoviesLinks(seedUrl);
+		let seedUrl;
+		while (this.seedUrls.length) {
+			try {
+				seedUrl = this.seedUrls.shift();
+				console.log(`Extracting links from: ${seedUrl}`);
+				const links = await this.extractMoviesLinks(seedUrl);
 
-        movieIDs.push(...links);
-      } catch (err) {
-        console.error(err.message);
-        this.seedUrls.push(seedUrl);
-      }
-    }
+				movieIDs.push(...links);
+			} catch (err) {
+				console.error(err.message);
+				this.seedUrls.push(seedUrl);
+			}
+		}
 
-    console.log(`Found ${movieIDs.length} links...`);
+		console.log(`Found ${movieIDs.length} links...`);
 
-    let movieID;
-    while (movieIDs.length) {
-      try {
-        movieID = movieIDs.shift();
-        console.log(`Requesting ${movieID}...`);
-        const details = await this.extractMovieDetails(movieID);
+		let movieID;
+		while (movieIDs.length) {
+			try {
+				movieID = movieIDs.shift();
+				console.log(`Requesting ${movieID}...`);
+				const details = await this.extractMovieDetails(movieID);
 
-        console.log('Got: ', details);
-        await esClient.index({
-          index: 'movies',
-          id: movieID.substring(2),
-          body: details
-        });
+				console.log('Got: ', details);
+				await esClient.index({
+					index: 'movies',
+					id: movieID.substring(2),
+					body: details
+				});
 
-        console.log(movieID, 'Indexed');
-        movieDetails.push(details);
-      } catch (err) {
-        console.error(err.message);
-        movieIDs.push(movieID);
-      }
-    }
+				console.log(movieID, 'Indexed');
+				movieDetails.push(details);
+			} catch (err) {
+				console.error(err.message);
+				movieIDs.push(movieID);
+			}
+		}
 
-    return movieDetails;
-  }
+		return movieDetails;
+	}
 }
 
 module.exports = IMDBCrawler;
